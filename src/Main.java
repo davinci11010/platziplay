fcc6910b0f183d8e1e83f717828db4f8296e1d04
aa <cceron086@soyudemedellin.edu.co>
2026-05-07 16:29:57 -0500

    add: primer commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Universidad de Medellin");
+        System.out.println("ESTE CODIGO SE VA PARA GITHUB");
+    }
+}
